0fc00d857aab87f1b8df208811974e2199bb5a18
Ducky <124077142+ducky4life@users.noreply.github.com>
2024-12-12 19:34:06 +0800

    better print messages, progress tracking

--- main.py
+++ main.py
@@ -8,23 +8,29 @@ api = nationstates.Nationstates("puppet dbid finder by Ducky used by") # put you
 
 
 mode = input("press enter if you would like the output to be a text file with only the IDs (use this if you're using my website), or type 'csv' for a csv file in 'nation,id' format: ")
+rowcount = 0
+rows = len(list(csv.reader(open('puppet.csv'))))
 
 if mode.lower() == "csv":
-	print("writing to csv file")
+	print("writing to csv file, please wait")
 	with open('puppet.csv') as csv_file:
 		csv_reader = csv.reader(csv_file)
 		output_csv = open("output.csv", "w")
 		for row in csv_reader:
+			rowcount+= 1
+			print(f"({rowcount}/{rows}) processing {row[0]}...")
 			nation = api.nation(row[0])
 			output_csv.write(f"{row[0]},{nation.dbid}\n")
 		print("finished! output is in output.csv")
 
 else:
-	print("writing to text file")
+	print("writing to text file, please wait")
 	with open('puppet.csv') as csv_file:
 		csv_reader = csv.reader(csv_file)
 		output_txt = open("output.txt", "w")
 		for row in csv_reader:
+			rowcount+= 1
+			print(f"({rowcount}/{rows}) processing {row[0]}...")
 			nation = api.nation(row[0])
 			output_txt.write(f"{nation.dbid}\n")
 		print("finished! output is in output.txt, paste the output in https://ducky4life.pages.dev/cardfinder/ to generate the card links!")
